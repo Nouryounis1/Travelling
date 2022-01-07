a6378dc3e5c732eed507f53795424858887f690a
Nour younis <76670143+Nouryounis1@users.noreply.github.com>
2022-01-07 12:57:01 +0200

    Update global_functions.dart

--- travel_app/lib/misc/global_functions.dart
+++ travel_app/lib/misc/global_functions.dart
@@ -4,4 +4,4 @@ void navigateTo(context, widget) =>
     Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
 
 void navigateAndFinish(context, widget) => Navigator.pushAndRemoveUntil(
-    context, MaterialPageRoute(builder: (context) => widget), (route) => false);
+    context, MaterialPageRoute(builder: (context) => widget), (route) => false); 
